10aedb35a67dfc5b0cabc02e768d9f6981f74811
Matcote <matthewcotesipod@gmail.com>
2020-08-06 13:14:26 -0400

    ex 5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { GlobalStyles } from "../GlobalStyles";
+import ArtistRoute from "../ArtistRoute";
+
+const DEFAULT_ARTIST_ID = "28j8lBWDdDSHSSt5oPlsX2";
 
 const App = () => {
   return (
     <Router>
       <GlobalStyles />
-      TODO
+      <Switch>
+        <Route exact={true} path="/">
+          <Redirect to={`/artists/${DEFAULT_ARTIST_ID}`} />
+        </Route>
+        <Route path="/artists/:id">
+          <ArtistRoute />
+        </Route>
+      </Switch>
     </Router>
   );
 };
